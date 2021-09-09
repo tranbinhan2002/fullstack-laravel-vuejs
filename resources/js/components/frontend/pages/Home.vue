a3e77a06ef18782bf943fac59c704e45baa7ba9b
tranbinhan1997 <tranbinhan20021997@gmail.com>
2021-09-09 12:59:59 +0700

    Frontend

---
+++ resources/js/components/frontend/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <app-slider></app-slider>
+    <app-banner></app-banner>
+  </div>
+</template>
+
+<script>
+import AppBanner from "../layouts/Banner.vue";
+import AppSlider from "../layouts/Slider.vue";
+export default {
+   components: { AppSlider, AppBanner },
+};
+</script>
+
+<style>
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -7,27 +7,34 @@ import Login from './components/auth/Login.vue';
 import Register from './components/auth/Register.vue';
 import Dashboard from './components/admins/Dashboard.vue';
 import NotFound from './components/NotFound.vue';
-import Home from './components/admins/pages/Home.vue';
-import Dashboard_Frontend from './components/frontend/Dashboard.vue';
 
+//Home
+import Home from './components/admins/pages/Home.vue';
 //Profile
 import Profile from './components/admins/pages/profile/index.vue';
 import Profile_Edit from './components/admins/pages/profile/edit.vue';
-
 //User
 import User from './components/admins/pages/users/index.vue';
 import User_Create from './components/admins/pages/users/create.vue';
 import User_Edit from './components/admins/pages/users/edit.vue';
-
 //Category
 import Category from './components/admins/pages/categories/index.vue';
 import Category_Create from './components/admins/pages/categories/create.vue';
 import Category_Edit from './components/admins/pages/categories/edit.vue';
-
 //Product
 import Product from './components/admins/pages/products/index.vue';
 import Product_Create from './components/admins/pages/products/create.vue';
 import Product_Edit from './components/admins/pages/products/edit.vue';
+
+//Frontend
+import Dashboard_Frontend from './components/frontend/Dashboard.vue';
+import Home_Frontend from './components/frontend/pages/Home.vue';
+import Shop from './components/frontend/pages/Shop.vue';
+import Blog from './components/frontend/pages/Blog.vue';
+import About from './components/frontend/pages/About.vue';
+import Contact from './components/frontend/pages/Contact.vue';
+import Cart from './components/frontend/pages/Cart.vue';
+import ProductDetail from './components/frontend/pages/ProductDetail.vue';
 
 import axios from 'axios';
 const router = new VueRouter({  
@@ -131,9 +138,43 @@ const router = new VueRouter({
         //User
         {
             path: '/',
-            name: 'home',
             component: Dashboard_Frontend,
-            children: [     
+            children: [
+                {
+                    path: '/',
+                    name: 'home',
+                    component: Home_Frontend
+                },
+                {
+                    path: '/shop',
+                    name: 'shop',
+                    component: Shop
+                },
+                {
+                    path: '/blog',
+                    name: 'blog',
+                    component: Blog
+                },  
+                {
+                    path: '/about',
+                    name: 'about',
+                    component: About
+                },
+                {
+                    path: '/contact',
+                    name: 'contact',
+                    component: Contact
+                },  
+                {
+                    path: '/cart',
+                    name: 'cart',
+                    component: Cart
+                }, 
+                {
+                    path: '/product-detail',
+                    name: 'product-detail',
+                    component: ProductDetail
+                },  
             ]
         }
     ]
